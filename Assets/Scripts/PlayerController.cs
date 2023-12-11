3debe2eb9feff8dfe0ebea92e11f49fe8b32ee1a
Thomaspls-unity <152604731+Thomaspls-unity@users.noreply.github.com>
2023-12-11 15:50:14 +0000

    Challenge 3.1
    -Added movement along the Z axis
    -Created an if statement for boundaries within the Z axis.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
+    public Vector3 moveDirection;
     public float speed = 10.0f;
     public float xRange = 15;
+    public float zRange = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -23,7 +27,13 @@ public class PlayerController : MonoBehaviour
     private void MovePlayer()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized; // Calculate the movement direction after updating input values
+
+        Vector3 movement = moveDirection * speed * Time.deltaTime; // Combine horizontal and vertical inputs and normalize the result
+
+        transform.Translate(movement); // Move the player based on the combined input
     }
 
     private void KeepPlayerInBounds() //If the position in x is greater than or less than to the xRange then the players position is reset to the lock x position
@@ -36,6 +46,16 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        if (transform.position.z < 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         }
     }
 }
