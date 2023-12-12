435b3d87c1280db8aef3aa572159a4d736004169
Thomaspls-unity <152604731+Thomaspls-unity@users.noreply.github.com>
2023-12-12 14:55:47 +0000

    Start of 3.2 Challenege
    -Added spawnboxes for the animals to use
    -Started to create a script for the random number generation to choose a spawnbox for the animals
    -Referenced each animal in the script

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    //public Transform[] spawnTransformsLeft; //Used for Challenge but not sure how to implement
+    //public Transform[] spawnTransformsRight;
+    //public GameObject animalCow;
+    //public GameObject animalMoose;
+    //public GameObject animalFox;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class DestroyOutOfBounds : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        DestroyObjects();
+    }
+
+    private void DestroyObjects()
     {
         if (transform.position.z > topBound)
         {
@@ -24,4 +34,14 @@ public class DestroyOutOfBounds : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    //public int GetRandomNumberLeft() //Used for Challenge but not sure how to implement
+    //{
+    //    int randomNumber = Random.Range(0, spawnTransformsLeft.Length);
+    //    return randomNumber;
+    //}
+    //public int GetRandomNumberRight()
+    //{
+    //    int randomNumber = Random.Range(0, spawnTransformsRight.Length);
+    //    return randomNumber;
+    //}
 }
